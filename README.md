554fe4dd12c88bd4e06c48b82172f4d66868138d
ndfsa <25588077+ndfsa@users.noreply.github.com>
2021-12-10 00:13:14 -0400

    single file renaming now works
    Now it would be cool if directories are handled without crashing or even
    recursively.
    
    Hashing part was working but it might have to be rebuilt.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # img-cmp
-This will hopefully help with removing similar images in a large library.
+This will hopefully help with identifying and removing similar images in a large library.
 
 ## What does it do?
 I planned this project to give a list of the n image pairs that are the most similar to each other,
-then rename the files to the image hash obtained, or save it in a cache file.
+then save it in a cache file, additionally, there is now an option to rename files in a library to
+their respective sha-1 hash (I thought it would be nice to have a consistent naming scheme).
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
 // use img_hash::{HashAlg, HasherConfig};
-use sha1::{Digest, Sha1};
+use sha1::Digest;
 use std::{
     env, fs,
     io::{self, Result},
+    path::PathBuf,
 };
 
-fn main() {
+fn main() -> Result<()> {
     let mut args = env::args();
 
     if args.len() > 1 {
         args.next();
-    } else {
-        panic!("No arguments");
     }
 
     match &args.next().expect("No command was provided")[..] {
-        "rename" => {
-            rename(args.next().expect("No file or folder provided"))
-                .expect("Error processing files");
-        }
-        "run" => {
-            run(args.next().expect("No file or folder provided"));
-        }
-        x => panic!("Unknown command: {}", x),
+        "rename" => rename(args.next().expect("No file or folder provided")),
+        "run" => run(args.next().expect("No file or folder provided")),
+        _ => Result::Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Command not recognized",
+        )),
     }
 }
-fn run(item: String) {
+fn run(_item: String) -> Result<()> {
     // for item in args {
     //     let image = image::open(&item).unwrap();
     //     let hasher = HasherConfig::new()
@@ -36,49 +33,53 @@ fn run(item: String) {
     //     let b64 = hash.to_base64().replace("+", "-").replace("/", "_");
     //     println!("{}: {}", &item, &b64);
     // }
+    Ok(())
 }
 
 fn rename(item: String) -> Result<()> {
+    let inner_aux = |file_path: &mut PathBuf| -> Result<()> {
+        let ext = file_path
+            .extension()
+            .expect("Cannot find extension")
+            .to_owned();
+
+        let old_path = file_path
+            .to_str()
+            .expect("Could not unpack path")
+            .to_owned();
+
+        let mut file = fs::File::open(&old_path)?;
+        let mut hasher = sha1::Sha1::new();
+
+        io::copy(&mut file, &mut hasher)?;
+
+        file_path.set_file_name(format!("{:x}", hasher.finalize()));
+        file_path.set_extension(ext);
+        let new_path = file_path
+            .to_str()
+            .expect("Could not unpack path")
+            .to_owned();
+
+        fs::rename(&old_path, &new_path)?;
+        println!("\nold path: {}\nnew path: {}", &old_path, &new_path);
+        Ok(())
+    };
+
     if fs::metadata(&item).unwrap().is_dir() {
         for entry in fs::read_dir(&item)? {
-            let mut file_entry = entry?.path();
-
-            if file_entry.is_dir() {
+            let mut file_path = entry?.path();
+            if file_path.is_dir() {
                 return Result::Err(io::Error::new(
                     io::ErrorKind::InvalidInput,
                     "Path points to directory",
                 ));
             }
-
-            let ext = file_entry
-                .extension()
-                .expect("Cannot find extension")
-                .to_owned();
-
-            let old_path = file_entry
-                .to_str()
-                .expect("Could not unpack path")
-                .to_owned();
-
-            let mut file = fs::File::open(&old_path)?;
-            let mut hasher = sha1::Sha1::new();
-
-            io::copy(&mut file, &mut hasher)?;
-
-            file_entry.set_file_name(format!("{:x}", hasher.finalize()));
-            file_entry.set_extension(ext);
-            let new_path = file_entry
-                .to_str()
-                .expect("Could not unpack path")
-                .to_owned();
-
-            fs::rename(&old_path, &new_path)?;
-            println!("\nold path: {}\nnew path: {}", &old_path, &new_path);
+            inner_aux(&mut file_path)?;
         }
         Ok(())
     } else {
-        let file = fs::File::open(item)?;
-        println!("success");
+        let mut file_path = PathBuf::from(item);
+        inner_aux(&mut file_path)?;
         Ok(())
     }
 }

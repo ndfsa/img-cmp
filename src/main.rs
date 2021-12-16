3e5b1f6767f0d43ffc0c6f900a2094dbe45640e0
ndfsa <25588077+ndfsa@users.noreply.github.com>
2021-12-15 21:08:19 -0400

    rewrote everything
    BUG: not keeping extensions
    DO NOT rename your files with this commit

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,96 @@
 // use img_hash::{HashAlg, HasherConfig};
 use sha1::Digest;
 use std::{
-    env, fs,
-    io::{self, Result},
-    path::PathBuf,
+	env, fs,
+	io::{self, Result},
+	path::{Path, PathBuf},
 };
 
 fn main() -> Result<()> {
-    let mut args = env::args();
+	let args: Vec<String> = env::args().collect();
 
-    if args.len() > 1 {
-        args.next();
-    }
+	if args.len() > 2 {
+		return handle_paths(args[2..].to_vec(), parse_cmd(&args[1]));
+	}
 
-    match &args.next().expect("No command was provided")[..] {
-        "rename" => rename(args.next().expect("No file or folder provided")),
-        "run" => run(args.next().expect("No file or folder provided")),
-        _ => Result::Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "Command not recognized",
-        )),
-    }
-}
-fn run(_item: String) -> Result<()> {
-    // for item in args {
-    //     let image = image::open(&item).unwrap();
-    //     let hasher = HasherConfig::new()
-    //         .preproc_dct()
-    //         .hash_alg(HashAlg::Mean)
-    //         .to_hasher();
-    //     let hash = hasher.hash_image(&image);
-    //     let b64 = hash.to_base64().replace("+", "-").replace("/", "_");
-    //     println!("{}: {}", &item, &b64);
-    // }
-    Ok(())
+	Err(std::io::Error::new(
+		io::ErrorKind::InvalidInput,
+		"No arguments",
+	))
 }
 
-fn rename(item: String) -> Result<()> {
-    let inner_aux = |file_path: &mut PathBuf| -> Result<()> {
-        let ext = file_path
-            .extension()
-            .expect("Cannot find extension")
-            .to_owned();
+fn parse_cmd(cmd: &str) -> fn(&str) -> Result<()> {
+	match cmd {
+		"rename" => rename,
+		"run" => run,
+		_ => panic!("Unknown command {}", cmd),
+	}
+}
 
-        let old_path = file_path
-            .to_str()
-            .expect("Could not unpack path")
-            .to_owned();
+fn run(_item: &str) -> Result<()> {
+	// for item in args {
+	//     let image = image::open(&item).unwrap();
+	//     let hasher = HasherConfig::new()
+	//         .preproc_dct()
+	//         .hash_alg(HashAlg::Mean)
+	//         .to_hasher();
+	//     let hash = hasher.hash_image(&image);
+	//     let b64 = hash.to_base64().replace("+", "-").replace("/", "_");
+	//     println!("{}: {}", &item, &b64);
+	// }
+	Ok(())
+}
 
-        let mut file = fs::File::open(&old_path)?;
-        let mut hasher = sha1::Sha1::new();
+fn handle_paths(args: Vec<String>, func: fn(&str) -> Result<()>) -> Result<()> {
+	for elem in args {
+		let path = Path::new(&elem);
+		if !path.exists() {
+			println!("\"{}\" not found", path.to_str().unwrap());
+			continue;
+		}
 
-        io::copy(&mut file, &mut hasher)?;
+		let path = path.canonicalize()?;
+		if path.is_dir() {
+			handle_paths(
+				path.read_dir()?
+					.map(|sub_elem| {
+						sub_elem
+							.unwrap()
+							.path()
+							.into_os_string()
+							.into_string()
+							.unwrap()
+					})
+					.collect(),
+				func.clone(),
+			)?;
 
-        file_path.set_file_name(format!("{:x}", hasher.finalize()));
-        file_path.set_extension(ext);
-        let new_path = file_path
-            .to_str()
-            .expect("Could not unpack path")
-            .to_owned();
+		// one day, symlinks will be supported
+		// } else if path.is_symlink() {
+		//     handle_paths(vec![path.into_os_string().into_string().unwrap()]);
+		// }
+		} else {
+			func(path.to_str().unwrap())?;
+		}
+	}
+	Ok(())
+}
 
-        fs::rename(&old_path, &new_path)?;
-        println!("\nold path: {}\nnew path: {}", &old_path, &new_path);
-        Ok(())
-    };
+fn rename(item: &str) -> Result<()> {
+	let path = Path::new(item);
+	let mut file = fs::File::open(&path)?;
 
-    if fs::metadata(&item).unwrap().is_dir() {
-        for entry in fs::read_dir(&item)? {
-            let mut file_path = entry?.path();
-            if file_path.is_dir() {
-                println!("Element is directory, skipping");
-                continue;
-            }
-            inner_aux(&mut file_path)?;
-        }
-        Ok(())
-    } else {
-        let mut file_path = PathBuf::from(item);
-        inner_aux(&mut file_path)?;
-        Ok(())
-    }
+	let mut hasher = sha1::Sha1::new();
+
+	io::copy(&mut file, &mut hasher)?;
+
+	let new_path = path.with_file_name(format!("{:x}", hasher.finalize()));
+	fs::rename(&path, &new_path)?;
+	println!(
+		"\nold path: {}\nnew path: {}",
+		path.to_str().unwrap(),
+		new_path.to_str().unwrap()
+	);
+
+	Ok(())
 }

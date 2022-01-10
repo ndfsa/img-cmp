5256cd761ccdf12e3aad67efa04164c5939efbb5
ndfsa <25588077+ndfsa@users.noreply.github.com>
2022-01-10 00:17:16 -0400

    finished mvp

--- README.md
+++ README.md
@@ -2,6 +2,39 @@
 This will hopefully help with identifying and removing similar images in a large library.
 
 ## What does it do?
-I planned this project to give a list of the n image pairs that are the most similar to each other,
-then save it in a cache file, additionally, there is now an option to rename files in a library to
-their respective sha-1 hash (I thought it would be nice to have a consistent naming scheme).
+Usage:
+
+```
+img-cmp COMMAND ITEM1 [ITEM2 [...]]
+```
+
+## Commands
+
+### rename
+For consistent naming, rename file to sha-1 hash output:
+
+```
+a_very_cool_name.xyz => 12aa5b72b1022c5db40a4f4424e1bb18a488abfe.xyz
+```
+
+Just in case you accidentally rename something that should not be renamed, the terminal output will
+contain the old and the new name.
+
+BE CAREFUL WITH THIS COMMAND, IT WILL RENAME EVERY FILE YOU FEED IT.
+
+For safer renaming give it individual files or a glob:
+
+```
+img-cmp rename *.png
+```
+
+### run
+Check for similar images according to their image hash, and save a cache file to shell's CWD.
+
+The program will always look for a cache file in your shell's CWD
+
+The cache file is named "cache.json" (/I may move this to XDG\_CACHE\_HOME/)
+
+### cache: you may want to
+If you deleted files from your library run this command, otherwise the program will compare
+non-existent files and maybe give false positives.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use img_hash::{HashAlg, Hasher, HasherConfig};
+use colored::*;
+use img_hash::{HashAlg, Hasher, HasherConfig, ImageHash};
+use serde::Deserialize;
 use sha1::Digest;
 use std::{
-    env, fs,
-    io::{self, Result},
+    collections::HashMap,
+    env,
+    fs::{self, File},
+    io::{self, Error, Result},
     path::Path,
 };
 
@@ -10,8 +14,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 2 {
-        match parse_cmd(&args[1], args[2..].to_vec()) {
-            Ok(()) => println!("Process finished"),
+        match parse_cmd(&args[1], flatten_list(args[2..].to_vec())) {
+            Ok(()) => (),
             Err(e) => println!("{}", e.to_string()),
         }
     }
@@ -20,60 +24,43 @@ fn main() {
 fn parse_cmd(cmd: &str, path_list: Vec<String>) -> Result<()> {
     match cmd {
         "rename" => {
-            for elem in flatten_list(path_list) {
+            for elem in path_list {
                 rename(&elem)?;
             }
         }
-        "run" => {
-            let hasher = HasherConfig::new()
-                .preproc_dct()
-                .hash_alg(HashAlg::Mean)
-                .to_hasher();
-            for elem in flatten_list(path_list) {
-                run(&elem, &hasher)?;
-            }
-        }
+        "run" => run(path_list)?,
+        "cache" => cache(path_list)?,
         _ => panic!("Unknown command {}", cmd),
     }
     Ok(())
 }
 
-fn run(item: &str, hasher: &Hasher) -> Result<()> {
-    let image = image::open(&item).unwrap();
-    let hash = hasher.hash_image(&image);
-    let b64 = hash.to_base64().replace("+", "-").replace("/", "_");
-    println!("{}: {}", &item, &b64);
+fn cache(path_list: Vec<String>) -> Result<()> {
+    let hasher = HasherConfig::new()
+        .preproc_dct()
+        .hash_alg(HashAlg::Mean)
+        .to_hasher();
+    let mut file_list = load_cache();
+    for elem in &path_list {
+        cache_elem(&elem, &hasher, &mut file_list)?;
+    }
+    trim_cache(&mut file_list)?;
+    save_cache(file_list);
     Ok(())
 }
 
-fn flatten_list(args: Vec<String>) -> Vec<String> {
-    let mut result = vec![];
-    for elem in args {
-        let path = Path::new(&elem);
-        if !path.exists() {
-            println!("\"{}\" not found", path.to_str().unwrap());
-            continue;
-        }
-
-        let path = path.canonicalize().unwrap();
-        if path.is_dir() {
-            result.append(&mut flatten_list(
-                path.read_dir()
-                    .unwrap()
-                    .map(|sub_elem| {
-                        String::from(sub_elem.expect("Cannot get file").path().to_string_lossy())
-                    })
-                    .collect(),
-            ));
-
-        // one day, symlinks will be supported
-        // } else if path.is_symlink() {
-        //     handle_paths(vec![path.into_os_string().into_string().unwrap()]);
-        } else {
-            result.push(path.into_os_string().into_string().unwrap());
-        }
+fn run(path_list: Vec<String>) -> Result<()> {
+    let hasher = HasherConfig::new()
+        .preproc_dct()
+        .hash_alg(HashAlg::Mean)
+        .to_hasher();
+    let mut file_list = load_cache();
+    for elem in path_list {
+        cache_elem(&elem, &hasher, &mut file_list)?;
     }
-    result
+    find_duplicates(&file_list);
+    save_cache(file_list);
+    Ok(())
 }
 
 fn rename(item: &str) -> Result<()> {
@@ -101,4 +88,106 @@ fn rename(item: &str) -> Result<()> {
     }
 
     Ok(())
+}
+
+fn trim_cache(file_list: &mut HashMap<String, String>) -> Result<()> {
+    for elem in file_list.clone() {
+        let path = Path::new(&elem.0);
+        if !path.exists() {
+            file_list.remove(&elem.0);
+            println!("Removed {} from cache", elem.0.red());
+        }
+    }
+    Ok(())
+}
+
+fn find_duplicates(file_list: &HashMap<String, String>) {
+    let mut lookup = file_list.clone();
+    for elem1 in file_list {
+        lookup.remove(elem1.0);
+        if let Ok(hash1) = ImageHash::<Box<[u8]>>::from_base64(elem1.1) {
+            for elem2 in &lookup {
+                if let Ok(hash2) = ImageHash::<Box<[u8]>>::from_base64(elem2.1) {
+                    let diff = hash1.dist(&hash2);
+                    if diff < 2 {
+                        println!("{} {} {}", elem1.0, elem2.0, diff);
+                    }
+                } else {
+                    println!("Could not read hash: {} from file {}", elem2.1, elem2.0)
+                }
+            }
+        } else {
+            println!("Could not read hash: {} from file {}", elem1.1, elem1.0)
+        }
+    }
+}
+
+fn save_cache(files: HashMap<String, String>) {
+    if let Ok(file) = File::create("./cache.json") {
+        serde_json::to_writer(&file, &files).unwrap();
+        // match serde_json::to_writer(&file, &files) {
+        //     Ok(_) => println!("{}", "Saved cache".green()),
+        //     Err(_) => println!("{}", "Could not save cahce".red()),
+        // }
+    }
+}
+
+fn load_cache() -> HashMap<String, String> {
+    if let Ok(file) = File::open("./cache.json") {
+        let mut de = serde_json::Deserializer::from_reader(file);
+        HashMap::<String, String>::deserialize(&mut de).unwrap_or(HashMap::new())
+    } else {
+        // println!("{}", "Could not find cache file".red());
+        HashMap::new()
+    }
+}
+
+fn cache_elem(item: &str, hasher: &Hasher, file_list: &mut HashMap<String, String>) -> Result<()> {
+    if file_list.contains_key(item) {
+        // println!("{}: {}", &item.bright_green(), file_list[item]);
+        return Ok(());
+    } else {
+        return match image::open(&item) {
+            Ok(image) => {
+                let hash = hasher.hash_image(&image);
+                let b64 = hash.to_base64();
+                file_list.insert(String::from(item), String::from(&b64));
+                // println!("{}: {}", &item.bright_red(), &b64);
+                return Ok(());
+            }
+            Err(error) => Err(Error::new(io::ErrorKind::Other, error.to_string())),
+        };
+    }
+}
+
+fn flatten_list(args: Vec<String>) -> Vec<String> {
+    let mut result = vec![];
+    for elem in args {
+        let path = Path::new(&elem);
+        if !path.exists() {
+            println!("\"{}\" not found", path.to_str().unwrap());
+            continue;
+        }
+
+        let path = path.canonicalize().unwrap();
+        if path.is_dir() {
+            result.append(&mut flatten_list(
+                path.read_dir()
+                    .unwrap()
+                    .map(|sub_elem| {
+                        String::from(sub_elem.expect("Cannot get file").path().to_string_lossy())
+                    })
+                    .collect(),
+            ));
+
+        // one day, symlinks will be supported
+        // } else if path.is_symlink() {
+        //     handle_paths(vec![path.into_os_string().into_string().unwrap()]);
+        } else {
+            if path.file_name().unwrap() != "cache.json" {
+                result.push(path.into_os_string().into_string().unwrap());
+            }
+        }
+    }
+    result
 }
